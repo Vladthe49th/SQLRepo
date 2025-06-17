a9bddb2c78d77f807bf86c65a51656f9cfbea93f
User <kenobi88@gmail.com>
2025-06-17 13:10:04 +0300

    1-commit

---
+++ API-Home1/API-Home1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+
+// Main task
+class Program
+{
+    // Messagebox import
+    [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+    public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+    const uint MB_YESNO = 0x00000004;
+    const uint MB_YESNOCANCEL = 0x00000003;
+    const uint MB_ICONQUESTION = 0x00000020;
+    const uint MB_ICONINFORMATION = 0x00000040;
+
+    const int IDYES = 6;
+    const int IDNO = 7;
+    const int IDCANCEL = 2;
+
+    static void Main()
+    {
+        do
+        {
+            PlayGame();
+        } while (WantsToPlayAgain());
+    }
+
+    static void PlayGame()
+    {
+        int min = 0, max = 100;
+        int guess;
+        int response;
+
+        MessageBox(IntPtr.Zero, "Make up a bumber from 0 to 100, then press OK", "Guess the num", MB_ICONINFORMATION);
+
+        while (true)
+        {
+            guess = (min + max) / 2;
+            response = MessageBox(IntPtr.Zero, $"Your number is: {guess}?\n\nYes - guessed\nNo - I`ll give a clue", "Attempt", MB_YESNO | MB_ICONQUESTION);
+
+            if (response == IDYES)
+            {
+                MessageBox(IntPtr.Zero, "Yay! I, the computer, guessed your number.", "Victory", MB_ICONINFORMATION);
+                break;
+            }
+            else
+            {
+                // Is the num bigger?
+                response = MessageBox(IntPtr.Zero, "Is your num bigger?", "Clue", MB_YESNO | MB_ICONQUESTION);
+
+                if (response == IDYES)
+                {
+                    min = guess + 1;
+                }
+                else
+                {
+                    max = guess - 1;
+                }
+
+                if (min > max)
+                {
+                    MessageBox(IntPtr.Zero, "I think there`s a mess in your head, let`s try again", "Error", MB_ICONINFORMATION);
+                    break;
+                }
+            }
+        }
+    }
+
+    static bool WantsToPlayAgain()
+    {
+        int response = MessageBox(IntPtr.Zero, "Wanna play again?", "Repeat", MB_YESNO | MB_ICONQUESTION);
+        return response == IDYES;
+    }
+
+
+}
+
+
+
---
+++ Entity-Home10/Entity-Home10/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+class Program
+{
+    //  STARTUPINFO structure
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct STARTUPINFO
+    {
+        public int cb;
+        public string lpReserved;
+        public string lpDesktop;
+        public string lpTitle;
+        public int dwX, dwY, dwXSize, dwYSize;
+        public int dwXCountChars, dwYCountChars;
+        public int dwFillAttribute;
+        public int dwFlags;
+        public short wShowWindow;
+        public short cbReserved2;
+        public IntPtr lpReserved2;
+        public IntPtr hStdInput, hStdOutput, hStdError;
+    }
+
+    // Process information structure
+    [StructLayout(LayoutKind.Sequential)]
+    public struct PROCESS_INFORMATION
+    {
+        public IntPtr hProcess;
+        public IntPtr hThread;
+        public uint dwProcessId;
+        public uint dwThreadId;
+    }
+
+    // WinAPI funcs
+    [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+    static extern bool CreateProcess(
+        string lpApplicationName,
+        string lpCommandLine,
+        IntPtr lpProcessAttributes,
+        IntPtr lpThreadAttributes,
+        bool bInheritHandles,
+        uint dwCreationFlags,
+        IntPtr lpEnvironment,
+        string lpCurrentDirectory,
+        ref STARTUPINFO lpStartupInfo,
+        out PROCESS_INFORMATION lpProcessInformation
+    );
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern uint WaitForSingleObject(IntPtr hHandle, uint dwMilliseconds);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern bool GetExitCodeProcess(IntPtr hProcess, out uint lpExitCode);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern bool CloseHandle(IntPtr hObject);
+
+    const uint INFINITE = 0xFFFFFFFF;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Daughter process loading...");
+
+        var si = new STARTUPINFO();
+        si.cb = Marshal.SizeOf(si);
+        var pi = new PROCESS_INFORMATION();
+
+        // Executive file
+        string commandLine = "notepad.exe";
+
+        bool result = CreateProcess(
+            null,
+            commandLine,
+            IntPtr.Zero,
+            IntPtr.Zero,
+            false,
+            0,
+            IntPtr.Zero,
+            null,
+            ref si,
+            out pi
+        );
+
+        if (!result)
+        {
+            Console.WriteLine("Process error: " + Marshal.GetLastWin32Error());
+            return;
+        }
+
+        // Wait for process to finish
+        WaitForSingleObject(pi.hProcess, INFINITE);
+
+        // Получаем код завершения
+        if (GetExitCodeProcess(pi.hProcess, out uint exitCode))
+        {
+            Console.WriteLine("Process finished with code: " + exitCode);
+        }
+        else
+        {
+            Console.WriteLine("No completion code has been received!.");
+        }
+
+
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+    }
+}
+
