45fd73bcc6deb4a38284f533fc1aaade501fd477
User <kenobi88@gmail.com>
2025-09-22 17:07:26 +0300

    1-commit

---
+++ ClientServer2/ClientServer2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    private const int DEFAULT_BUFLEN = 512;
+    private const string DEFAULT_PORT = "27015";
+
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.Title = "CLIENT SIDE";
+
+        try
+        {
+            var ipAddress = IPAddress.Loopback;
+            var remoteEndPoint = new IPEndPoint(ipAddress, int.Parse(DEFAULT_PORT));
+
+            var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientSocket.Connect(remoteEndPoint);
+
+            Console.WriteLine("Подключение к серверу установлено.");
+            Console.WriteLine("\nДоступные команды:");
+            Console.WriteLine("1) привет");
+            Console.WriteLine("2) как дела");
+            Console.WriteLine("3) время");
+            Console.WriteLine("4) любое целое число (сервер вернёт число+1)");
+            Console.WriteLine("5) exit (для выхода)\n");
+
+            while (true)
+            {
+                Console.Write("Введите сообщение: ");
+                string input = Console.ReadLine() ?? "";
+
+                byte[] messageBytes = Encoding.UTF8.GetBytes(input);
+                clientSocket.Send(messageBytes);
+
+                var buffer = new byte[DEFAULT_BUFLEN];
+                int bytesReceived = clientSocket.Receive(buffer);
+                string response = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+                Console.WriteLine($"Ответ от сервера: {response}");
+
+                if (input.ToLower() == "exit")
+                    break;
+            }
+
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+            Console.WriteLine("Соединение закрыто.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
---
+++ ClientServer2/Server/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    private const int DEFAULT_BUFLEN = 512;
+    private const string DEFAULT_PORT = "27015";
+
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.Title = "SERVER SIDE";
+        Console.WriteLine("Сервер запущен...");
+
+        try
+        {
+            var ipAddress = IPAddress.Any;
+            var localEndPoint = new IPEndPoint(ipAddress, int.Parse(DEFAULT_PORT));
+
+            var listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+
+            Console.WriteLine("Ожидание подключения клиента...");
+            var clientSocket = listener.Accept();
+            Console.WriteLine("Клиент подключился!");
+
+            while (true)
+            {
+                var buffer = new byte[DEFAULT_BUFLEN];
+                int bytesReceived = clientSocket.Receive(buffer);
+
+                if (bytesReceived > 0)
+                {
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+                    Console.WriteLine($"Сообщение от клиента: {message}");
+
+                    // Проверяем команды
+                    string response;
+                    if (message.ToLower() == "как дела")
+                        response = "Лучше всех";
+                    else if (message.ToLower() == "привет")
+                        response = "И тебе привет!";
+                    else if (message.ToLower() == "время")
+                        response = $"Сейчас: {DateTime.Now}";
+                    else if (message.ToLower() == "exit")
+                    {
+                        response = "Соединение закрывается...";
+                        clientSocket.Send(Encoding.UTF8.GetBytes(response));
+                        break;
+                    }
+                    else if (int.TryParse(message, out int number))
+                        response = (number + 1).ToString();
+                    else
+                        response = "Неизвестная команда";
+
+                    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                    clientSocket.Send(responseBytes);
+                    Console.WriteLine($"Ответ серверa: {response}");
+                }
+            }
+
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+            Console.WriteLine("Сервер завершил работу.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
+
