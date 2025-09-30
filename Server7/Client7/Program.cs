969dcf434f37f4832190be5be7648d0e45619880
User <kenobi88@gmail.com>
2025-09-30 17:26:04 +0300

    1-commit

---
+++ Server7/Client7/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+class TcpChatClient
+{
+    private readonly string host;
+    private readonly int port;
+    private TcpClient? client;
+
+    public TcpChatClient(string host, int port)
+    {
+        this.host = host;
+        this.port = port;
+    }
+
+    public async Task ConnectAsync()
+    {
+        client = new TcpClient();
+        await client.ConnectAsync(host, port);
+        Console.WriteLine("Підключено до сервера!");
+
+        _ = Task.Run(ReceiveMessagesAsync);
+        await SendMessagesAsync();
+    }
+
+    private async Task ReceiveMessagesAsync()
+    {
+        var buffer = new byte[1024];
+        var stream = client!.GetStream();
+
+        while (true)
+        {
+            int byteCount = await stream.ReadAsync(buffer, 0, buffer.Length);
+            if (byteCount == 0) break;
+
+            var message = Encoding.UTF8.GetString(buffer, 0, byteCount);
+            Console.WriteLine(message);
+        }
+    }
+
+    private async Task SendMessagesAsync()
+    {
+        var stream = client!.GetStream();
+
+        while (true)
+        {
+            var message = Console.ReadLine();
+            if (message == "off") break;
+
+            var data = Encoding.UTF8.GetBytes(message);
+            await stream.WriteAsync(data, 0, data.Length);
+        }
+
+        client.Close();
+        Console.WriteLine("Відключено від сервера.");
+    }
+}
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        var client = new TcpChatClient("127.0.0.1", 9000);
+        await client.ConnectAsync();
+    }
+}
---
+++ Server7/Server7/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class TcpChatServer
+{
+    private readonly int port;
+    private readonly List<TcpClient> clients = new();
+    private readonly StringBuilder messageHistory = new();
+
+    public TcpChatServer(int port)
+    {
+        this.port = port;
+    }
+
+    public async Task StartAsync()
+    {
+        var listener = new TcpListener(IPAddress.Any, port);
+        listener.Start();
+        Console.WriteLine($"Сервер запущено на порту {port}");
+
+        while (true)
+        {
+            var client = await listener.AcceptTcpClientAsync();
+            lock (clients) clients.Add(client);
+
+            Console.WriteLine("Новий клієнт підключився!");
+            _ = HandleClientAsync(client);
+        }
+    }
+
+    private async Task HandleClientAsync(TcpClient client)
+    {
+        var stream = client.GetStream();
+
+        // Надсилаємо історію новому клієнту
+        var history = Encoding.UTF8.GetBytes(messageHistory.ToString());
+        await stream.WriteAsync(history, 0, history.Length);
+
+        var buffer = new byte[1024];
+        while (true)
+        {
+            int byteCount;
+            try
+            {
+                byteCount = await stream.ReadAsync(buffer, 0, buffer.Length);
+                if (byteCount == 0) break; // клієнт відключився
+            }
+            catch
+            {
+                break;
+            }
+
+            var message = Encoding.UTF8.GetString(buffer, 0, byteCount);
+            var formattedMessage = $"[{DateTime.Now:T}] {message}";
+
+            Console.WriteLine(formattedMessage);
+            messageHistory.AppendLine(formattedMessage);
+            await BroadcastAsync(formattedMessage, client);
+        }
+
+        lock (clients) clients.Remove(client);
+        client.Close();
+    }
+
+    private async Task BroadcastAsync(string message, TcpClient sender)
+    {
+        var data = Encoding.UTF8.GetBytes(message);
+
+        lock (clients)
+        {
+            foreach (var c in clients)
+            {
+                if (c == sender) continue;
+                try
+                {
+                    c.GetStream().WriteAsync(data, 0, data.Length);
+                }
+                catch { }
+            }
+        }
+    }
+}
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        var server = new TcpChatServer(9000);
+        await server.StartAsync();
+    }
+}
